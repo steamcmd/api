d564f30858ecd88d6c2da9813893e3f61fd84dfd
Sam Alexander <sam@extendedplay.io>
2023-02-24 11:39:46 -0800

    Adds redis_connection() for credentials

--- src/functions.py
+++ src/functions.py
@@ -83,13 +83,11 @@ def cache_write(app_id, data):
     # return failed status
     return False
 
-
-def redis_read(app_id):
+def redis_connection():
     """
-    Read app info from Redis cache.
+    Parse redis config and connect.
     """
 
-    # parse redis config and connect
     # try connection string, or default to separate REDIS_* env vars
     if "REDIS_URL" in os.environ:
         rds = redis.Redis.from_url(os.environ["REDIS_URL"])
@@ -99,6 +97,16 @@ def redis_read(app_id):
             port=os.environ["REDIS_PORT"],
             password=os.environ["REDIS_PASSWORD"],
         )
+
+    # return connection
+    return rds
+
+def redis_read(app_id):
+    """
+    Read app info from Redis cache.
+    """
+
+    rds = redis_connection()
 
     try:
         # get info from cache
@@ -134,13 +142,8 @@ def redis_write(app_id, data):
     Write app info to Redis cache.
     """
 
-    # parse redis config and connect
-    rds = redis.Redis(
-        host=os.environ["REDIS_HOST"],
-        port=os.environ["REDIS_PORT"],
-        password=os.environ["REDIS_PASSWORD"],
-    )
-
+    rds = redis_connection()
+    
     # write cache data and set ttl
     try:
         # convert dict to json
